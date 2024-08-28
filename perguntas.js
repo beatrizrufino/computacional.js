1aaca5d16a9a15d3017182e4006652b7e46fe548
beatrizrufino <119423927+beatrizrufino@users.noreply.github.com>
2024-08-28 20:57:51 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é a sua atividade favorita no tempo livre?",
+        alternativas: [
+            {
+                texto: "Praticar esportes",
+                afirmacao: [
+                    "Pensador(a) Você é uma pessoa introspectiva e gosta de atividades que estimulam a mente. Prefere ambientes tranquilos e valoriza o tempo sozinho(a)."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Assistir a filmes ou séries",
+                afirmacao: [
+                    "Observador(a) Você é uma pessoa calma e analítica. Gosta de observar o mundo ao seu redor e prefere ambientes que permitam reflexão e contemplação."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você prefere passar um fim de semana?",
+        alternativas: [
+            {
+                texto: "Em casa, relaxando",
+                afirmacao: [
+                    "Pensador(a) Você é uma pessoa introspectiva e gosta de atividades que estimulam a mente. Prefere ambientes tranquilos e valoriza o tempo sozinho(a).",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Socializando com amigos",
+                afirmacao: [
+                    "Sociável Você é extrovertido(a) e adora estar rodeado(a) de pessoas. Valoriza as amizades e gosta de compartilhar momentos com os outros."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu animal favorito?",
+        alternativas: [
+            {
+                texto: "Gato",
+                afirmacao: [
+                    "Pensador(a) Você é uma pessoa introspectiva e gosta de atividades que estimulam a mente. Prefere ambientes tranquilos e valoriza o tempo sozinho(a)."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Cachorro",
+                afirmacao: [
+                    "Aventureiro(a) Você adora desafios e está sempre em busca de novas experiências. É enérgico(a) e gosta de estar em movimento."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você descreveria sua personalidade?",
+        alternativas: [
+            {
+                texto: "Equilibrado(a)",
+                afirmacao: [
+                    "Sociável Você é extrovertido(a) e adora estar rodeado(a) de pessoas. Valoriza as amizades e gosta de compartilhar momentos com os outros."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: " Reflexivo(a)",
+                afirmacao: [
+                    "Observador(a) Você é uma pessoa calma e analítica. Gosta de observar o mundo ao seu redor e prefere ambientes que permitam reflexão e contemplação. "
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você lida com desafios?",
+        alternativas: [
+            {
+                texto: "Penso em várias soluções possíveis",
+                afirmacao: [
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Enfrento de cabeça erguida",
+                afirmacao: [
+                    ""
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu estilo de viagem preferido?",
+        alternativas: [
+            {
+                texto: "Cultural",
+                afirmacao: [
+                    ""
+                ],
+            },
+            {
+                texto: "Aventura",
+                afirmacao: [
+                    ""
+                ],
+            },
+        ]
+    },
+];
